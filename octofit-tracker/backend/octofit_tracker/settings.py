d936181e56cdd392a51259fdd8bef4d9799473e1
Sayantani-biswas <sayantani.biswas@accenture.com>
2025-11-12 05:45:46 +0000

    Update ALLOWED_HOSTS and API URLs for Codespace compatibility and REST endpoint formatting

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-_+4(urskx=a$-hgr5x069s&19y=x(@&yb#b()nhs(^8-2s19d(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+# Dynamically set ALLOWED_HOSTS for localhost and Codespace
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
 
 
 # Application definition
@@ -86,10 +92,7 @@ DATABASES = {
         'CLIENT': {
             'host': 'localhost',
             'port': 27017,
-            'username': '',
-            'password': '',
-            'authSource': '',
-            'authMechanism': '',
+            # Remove empty username, password, authSource, authMechanism for local dev
         },
     }
 }
--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from .views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import os
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -28,14 +30,20 @@ router.register(r'activities', ActivityViewSet)
 router.register(r'leaderboard', LeaderboardViewSet)
 router.register(r'workouts', WorkoutViewSet)
 
+
 @api_view(['GET'])
 def api_root(request):
+    CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+    if CODESPACE_NAME:
+        base_url = f"https://{CODESPACE_NAME}-8000.app.github.dev/api/"
+    else:
+        base_url = "http://localhost:8000/api/"
     return Response({
-        'users': '/users/',
-        'teams': '/teams/',
-        'activities': '/activities/',
-        'leaderboard': '/leaderboard/',
-        'workouts': '/workouts/',
+        'users': f'{base_url}users/',
+        'teams': f'{base_url}teams/',
+        'activities': f'{base_url}activities/',
+        'leaderboard': f'{base_url}leaderboard/',
+        'workouts': f'{base_url}workouts/',
     })
 
 urlpatterns = [
